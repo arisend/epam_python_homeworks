38250fb8d0b1cb479d256e6e35a888e909ff1551
arisend <64369075+arisend@users.noreply.github.com>
2021-11-29 00:53:44 +0300

    Update save_original_info.py

--- homework_05/hw/save_original_info.py
+++ homework_05/hw/save_original_info.py
@@ -18,6 +18,7 @@ print(custom_sum.__original_func)  # <function custom_sum at <some_id>>
 
 import functools
 
+
 def save_function_decorator(function_to_save):
     def wrapper(*args, **kwargs):
         "This is the wrapper function"
@@ -40,6 +41,7 @@ def print_result(func):
         result = func(*args, **kwargs)
         print(result)
         return result
+
     return wrapper
 
 
