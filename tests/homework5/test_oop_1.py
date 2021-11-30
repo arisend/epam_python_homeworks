47a6531266c2326a1840056704f21d6ca30506dd
arisend <64369075+arisend@users.noreply.github.com>
2021-11-30 12:05:41 +0300

    Update test_oop_1.py

--- tests/homework5/test_oop_1.py
+++ tests/homework5/test_oop_1.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pytest
 import datetime
-from homework_05.hw.oop_1 import Homework, Student, Teacher
+from homework_05.hw import Homework, Student, Teacher
 
 teacher = Teacher('Daniil', 'Shadrin')
 student = Student('Roman', 'Petrov')
