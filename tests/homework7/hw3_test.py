967d2f0d9a12839515680b13a7360dfd6b27a922
arisend <64369075+arisend@users.noreply.github.com>
2021-12-08 04:28:27 +0300

    homework7

--- tests/homework7/hw3_test.py
+++ tests/homework7/hw3_test.py
@@ -3,9 +3,9 @@ from homework_07.hw.hw3 import tic_tac_toe_checker
 
 def test_negative_case():
     """Testing wrong cases"""
-    assert tic_tac_toe_checker([['-', '-', 'o'],['-', 'o', 'o'],['x', 'x', 'x']]) != "o wins!"
+    assert tic_tac_toe_checker([['-', '-', 'o'], ['-', 'o', 'o'], ['x', 'x', 'x']]) != "o wins!"
 
 
 def test_positive_case():
     """Testing correct cases"""
-    assert tic_tac_toe_checker([['-', '-', 'o'], ['-', 'x', 'o'],['x', 'o', 'x']]) == "unfinished"
+    assert tic_tac_toe_checker([['-', '-', 'o'], ['-', 'x', 'o'], ['x', 'o', 'x']]) == "unfinished"
