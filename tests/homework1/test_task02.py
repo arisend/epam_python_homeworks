7fce91cfc7a5bba39d84ea008b9914b1a4447d87
arisend <64369075+arisend@users.noreply.github.com>
2021-12-10 02:25:13 +0300

    homework1-upd

--- tests/homework1/test_task02.py
+++ tests/homework1/test_task02.py
@@ -5,7 +5,6 @@ from homework_01.hw.task02 import check_fibonacci
 def test_positive_case():
     """Testing correct seq"""
     assert check_fibonacci([0, 1, 1, 2, 3, 5, 8])
-    #assert check_fibonacci([-3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8])
 
 def test_negative_case():
     """Testing wrong seq"""
