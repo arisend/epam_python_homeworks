590e3381a4c223e35695980b5c42709dce3fafc5
arisend <64369075+arisend@users.noreply.github.com>
2021-12-14 23:32:54 +0300

    homework8_task2

--- homework_08/hw/task2.py
+++ homework_08/hw/task2.py
@@ -1,61 +1,70 @@
 """
-Homework 2:
-============
-
-Preamble
-========
-
-We have a database file (example.sqlite) in sqlite3 format with some tables and data. All tables have 'name' column and maybe some additional ones.
-
-Data retrieval and modifications are done with sqlite3 module by issuing SQL statements.
-For example, to get all data from TABLE1::
-
-    import sqlite3
-    conn = sqlite3.connect('example.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * from TABLE1')
-    data = cursor.fetchall()   # will be a list with data.
-
-instead of getting all data at once, you can use .fetchone() calls and named expressions::
-
-    while row:=cursor.fetchone():
-        print(row)
-
-To get a row with specific name equal to some value::
-
-    import sqlite3
-    conn = sqlite3.connect('example.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * from presidents where name=:name', {name:'Yeltsin'})
-    data = cursor.fetchall()  # will get all records with this name. You can also use .fetchone() to get one record.
-
-in order to get record with first name (sorted alphabetically) use SQL expression `SELECT * from presidents order by name asc limit 1`
-in order to get record after specified (sorted alphabetically) use SQL expression `SELECT * from presidents where name > :name order by name limit`.
-To get amount of records in table TABLE1, use `select count(*) from TABLE1` query.
-
-
-Please refer to this documents for more information about how to retrieve data from sqlite database:
-DBAPI: https://www.python.org/dev/peps/pep-0249/
-sqlite3 module: https://docs.python.org/3/library/sqlite3.html
-
-
-Task
-====
-
-Write a wrapper class TableData for database table, that when initialized with database name and table acts as collection object (implements Collection protocol).
-Assume all data has unique values in 'name' column.
+Write a wrapper class TableData for database table, that when initialized with database name and table acts as
+collection object (implements Collection protocol). Assume all data has unique values in 'name' column.
 So, if presidents = TableData(database_name='example.sqlite', table_name='presidents')
 
 then
- -  `len(presidents)` will give current amount of rows in presidents table in database
- -  `presidents['Yeltsin']` should return single data row for president with name Yeltsin
- -  `'Yeltsin' in presidents` should return if president with same name exists in table
- -  object implements iteration protocol. i.e. you could use it in for loops::
-       for president in presidents:
-           print(president['name'])
- - all above mentioned calls should reflect most recent data. If data in table changed after you created collection instance, your calls should return updated data.
-
-Avoid reading entire table into memory. When iterating through records, start reading the first record, then go to the next one, until records are exhausted.
-When writing tests, it's not always neccessary to mock database calls completely. Use supplied example.sqlite file as database fixture file.
-
+len(presidents) will give current amount of rows in presidents table in database
+presidents['Yeltsin'] should return single data row for president with name Yeltsin
+'Yeltsin' in presidents should return if president with same name exists in table
+object implements iteration protocol. i.e. you could use it in for loops::
+for president in presidents:
+print(president['name'])
+all above mentioned calls should reflect most recent data. If data in table changed after you created collection
+instance, your calls should return updated data.
+Avoid reading entire table into memory. When iterating through records, start reading the first record, then go
+to the next one, until records are exhausted. When writing tests, it's not always neccessary to mock database calls
+completely. Use supplied example.sqlite file as database fixture file.
 """
+import sqlite3
+
+
+class TableData:
+    def __init__(self, database_name, table_name):
+        self.database_name = database_name
+        self.table_name = table_name
+
+    def __contains__(self, value):
+        self.conn = sqlite3.connect(self.database_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('SELECT COUNT (*) FROM {} where name="{}"'.format(self.table_name, value))
+        (ln,) = self.cursor.fetchall()[0]
+        self.conn.close()
+        return ln > 0
+
+    def __getitem__(self, item):
+        self.conn = sqlite3.connect(self.database_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('SELECT * from {} where name="{}"'.format(self.table_name, item))
+        while row := self.cursor.fetchone():
+            self.conn.close()
+            return row
+
+    def __iter__(self):
+        """Probably not the optimal solution to close connection at the end of iterations,
+        but maybe it will be enough to add notice of such behavior to documentation in order to avoid possible
+        confusions. and give advise to user to call Instance.conn_close() if they not going to iterate
+        until the end of table.
+        """
+        self.conn = sqlite3.connect(self.database_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('SELECT * from {}'.format(self.table_name))
+        colnames = self.cursor.description
+        while row := self.cursor.fetchone():
+            retdict = {}
+            for i, col in enumerate(colnames):
+                retdict[col[0]] = row[i]
+            yield retdict
+        self.conn.close()
+
+
+    def __len__(self):
+        self.conn = sqlite3.connect(self.database_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('SELECT COUNT (*) FROM {}'.format(self.table_name))
+        (ln,) = self.cursor.fetchall()[0]
+        self.conn.close()
+        return int(ln)
+
+    def conn_close(self):
+        self.conn.close()
