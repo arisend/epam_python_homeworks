0691d5424134bbe11b0f48e182980c9d7315fa05
arisend <64369075+arisend@users.noreply.github.com>
2021-12-14 23:37:59 +0300

    homework8

--- tests/homework8/test_task1.py
+++ tests/homework8/test_task1.py
@@ -1,12 +1,12 @@
 import os.path
 from homework_08.hw.task1 import KeyValueStorage
 
-path_to_file = os.path.join(os.path.dirname(__file__), 'data.txt')
+path_to_file = os.path.join(os.path.dirname(__file__), 'task1.txt')
 
 
 def test_positive_case():
     """Testing correct cases"""
-    storage = KeyValueStorage('task1.txt')
+    storage = KeyValueStorage(path_to_file)
 
     assert storage['name'] == 'kek'
     assert storage.song == 'shadilay'
@@ -15,5 +15,5 @@ def test_positive_case():
 
 def test_negative_case():
     """Testing wrong cases"""
-    storage = KeyValueStorage('task1.txt')
+    storage = KeyValueStorage(path_to_file)
     assert storage.last_name != ""
