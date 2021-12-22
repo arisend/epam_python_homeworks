a9e0cdb6e65604778082bf7341c39e87fe9def29
arisend <64369075+arisend@users.noreply.github.com>
2021-12-23 01:55:43 +0300

    Update test_parser_sp500.py

--- tests/homework10/test_parser_sp500.py
+++ tests/homework10/test_parser_sp500.py
@@ -4,16 +4,17 @@ from homework_10.hw.parser_sp500 import Parser_sp500
 
 path_to_file1 = os.path.join(os.path.dirname(__file__), 'top_highest_price.json')
 path_to_file2 = os.path.join(os.path.dirname(__file__), 'top_lowest_pe.json')
-
+path_to_file3 = os.path.join(os.path.dirname(__file__), 'top_with_max_growth.json')
+path_to_file4 = os.path.join(os.path.dirname(__file__), 'top_highest_potential_profit.json')
 
 parser = Parser_sp500()
-with open('top_highest_price.json', 'w') as file:
+with open(path_to_file1, 'w') as file:
     json.dump(parser.top_highest_price(), file)
-with open('top_lowest_pe.json', 'w') as file:
+with open(path_to_file2, 'w') as file:
     json.dump(parser.top_lowest_pe(), file)
-with open('top_with_max_growth.json', 'w') as file:
+with open(path_to_file3, 'w') as file:
     json.dump(parser.top_with_max_growth(), file)
-with open('top_highest_potential_profit.json', 'w') as file:
+with open(path_to_file4, 'w') as file:
     json.dump(parser.top_highest_potential_profit(), file)
 
 
@@ -22,10 +23,15 @@ def test_positive_case():
     with open(path_to_file1, 'r') as json_file:
         data = json.load(json_file)
         assert len(data['data']) == 10
-
+    with open(path_to_file3, 'r') as json_file:
+        data = json.load(json_file)
+        assert len(data['data']) == 10
 
 def test_negative_case():
     """Testing wrong cases"""
     with open(path_to_file2, 'r') as json_file:
+        data = json.load(json_file)
+        assert len(data['data']) != 0
+    with open(path_to_file4, 'r') as json_file:
         data = json.load(json_file)
         assert len(data['data']) != 0
