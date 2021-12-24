5fb756ea0ab737ce55038374e700bdc18ee27453
arisend <64369075+arisend@users.noreply.github.com>
2021-12-24 20:01:30 +0300

    Update test_save_original_info.py

--- tests/homework5/test_save_original_info.py
+++ tests/homework5/test_save_original_info.py
@@ -1,44 +1,23 @@
 import os
 import sys
 import io
-import functools
-import pytest
-import datetime
 
 from homework_05.hw.save_original_info import save_function_decorator, print_result, custom_sum
 
-custom_sum([1, 2, 3], [4, 5])
-custom_sum(1, 2, 3, 4)
-
-print(custom_sum.__doc__)
-print(custom_sum.__name__)
-without_print = custom_sum.__original_func
-print(custom_sum.__original_func)
-# the result returns without printing
-without_print(1, 2, 3, 4)
-
-
 def test_positive_case():
     """Testing correct case"""
-    old_stdout = sys.stdout  # Memorize the default stdout stream
-    sys.stdout = buffer = io.StringIO()
     custom_sum([1, 2, 3], [4, 5])
     custom_sum(1, 2, 3, 4)
-    print(custom_sum.__doc__)
-    print(custom_sum.__name__)
-    what_was_printed = buffer.getvalue()
-    assert what_was_printed == """[1, 2, 3, 4, 5]
-10
-This function can sum any objects which have __add___
-custom_sum
-"""
-    sys.stdout = old_stdout  # Put the old stream back in place
-    buffer.close()
+
+    with open(path_to_file_with_output, 'r') as f:
+        assert custom_sum.__doc__+ "\n" + custom_sum.__name__ == f.read()
+
 
 def test_negative_case():
     old_stdout = sys.stdout  # Memorize the default stdout stream
     sys.stdout = buffer = io.StringIO()
     print(custom_sum.__original_func)
+    without_print = custom_sum.__original_func
     without_print(1, 2, 3, 4)
     what_was_printed = buffer.getvalue()
     assert """<function custom_sum at""" in what_was_printed and '10\n' not in what_was_printed
